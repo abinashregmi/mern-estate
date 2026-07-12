3037794f5de4fc0a763fda3d07ed8221984e56f6
Abinash Regmi <abinashregmi01234@gmail.com>
2026-07-12 12:32:30 +0545

    complete delete function

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from 'react-redux';
 import { useRef, useState, useEffect } from 'react';
 import { supabase } from '../supabase';
-import { updateUserStart, updateUserSuccess, updateUserFailure } from '../redux/user/userSlice.js';
+import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess } from '../redux/user/userSlice.js';
 import { useDispatch } from 'react-redux';
 
 
@@ -84,6 +84,25 @@ useEffect(() => {
     }
   };
 
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+      
+    }
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>  
       <h1 className="text-3xl font-semibold text-center my-7 ">
@@ -130,10 +149,9 @@ useEffect(() => {
           </button>
       </form>
       <div className='flex justify-between mt-5'>
-        <span className='text-red-700 cursor-pointer'>Delete Account</span>
+        <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete Account</span>
         <span className='text-red-700 cursor-pointer'>Sign Out</span>
       </div>
-          <p className='text-red-700 mt-5'>{error ? error : ''}</p>
           <p className='text-green-700 mt-5'>{updateSuccess ? 'Profile updated successfully!' : ''}</p>
 
     </div>
